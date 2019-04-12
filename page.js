ea5b8a9e6f7c86aae11fb70df7936c393bf2871a
Metype <39036543+metype@users.noreply.github.com>
2019-04-12 18:24:04 -0500

    Update page.js

--- page.js
+++ page.js
@@ -3,5 +3,9 @@ createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
-ellipse(10,10,mnouseX,mouseY);
+ellipse(10,10,mouseX,mouseY);
 }
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+  }
