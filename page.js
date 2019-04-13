ec585f4831b2510360af3f65efc3539b372542fa
Metype <39036543+metype@users.noreply.github.com>
2019-04-12 19:04:24 -0500

    Update page.js

--- page.js
+++ page.js
@@ -3,10 +3,13 @@ let logo,bg;
 function setup() {
 createCanvas(windowWidth,windowHeight);
 logo=loadImage("https://raw.githubusercontent.com/metype/MetypeBot-Dashboard/master/MetypeBot-Logo.png");
-bg = loadImage();
+bg = loadImage("https://raw.githubusercontent.com/metype/MetypeBot-Dashboard/master/bgcolor.png");
 }
 
 function draw() {
+    imageMode(CORNER);
+    bg.resize(width,logo.height*2);
+    image(bg,0,0);
     imageMode(CENTER);
 image(logo,width/2,logo.height);
 }
