4a5e508324aec6a55f218b778f8e525e651bac9e
Metype <39036543+metype@users.noreply.github.com>
2019-07-04 12:08:02 -0500

    Update page.js

--- page.js
+++ page.js
@@ -5,7 +5,7 @@ function setup() {
     var cnv = createCanvas(windowWidth, windowHeight);
     cnv.style('display', 'block');
     loc=window.location;
-    loc = loc.split("?code");
+    loc = loc.split("?code")[1];
     console.log(loc);
 logo=loadImage("https://raw.githubusercontent.com/metype/MetypeBot-Dashboard/master/MetypeBot-Logo.png");
 bg = loadImage("https://raw.githubusercontent.com/metype/MetypeBot-Dashboard/master/bgcolor.png");
