dc4cbec2287662b92882ae760919ac5d08e78737
Metype <39036543+metype@users.noreply.github.com>
2019-07-26 15:32:56 -0500

    Update maze.js

--- maze.js
+++ maze.js
@@ -1,5 +1,5 @@
 var pathname = window. location. pathname; console. log(pathname);
-const maze = pathname.split("?")[2];
+const maz = pathname.split("?")[2];
 const width = pathname.split("?")[1];
 let maze;
 
@@ -7,7 +7,7 @@ function setup(){
 createCanvas(windowWidth,windowHeight);
   for(let i=0;i<width;i++){
     for(int j=0;j<15;j++){
-      maze[i][j] = maze.substring(i+(j*15),(i+(j*15)+1))
+      maze[i][j] = maz.substring(i+(j*15),(i+(j*15)+1))
     }
   }
   console.log(maze);
