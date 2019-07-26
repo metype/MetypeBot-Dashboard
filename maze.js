945a1897794d50b5db7a8fdf6a4e6755744e421a
Metype <39036543+metype@users.noreply.github.com>
2019-07-26 16:29:42 -0500

    Update maze.js

--- maze.js
+++ maze.js
@@ -1,18 +1,60 @@
-var pathname = window. location. pathname; console. log(pathname);
-const maz = pathname.split("?")[2];
-const width = pathname.split("?")[1];
-let maze;
+var pathname = window. location. href; console. log(pathname);
+const maz = pathname.split("?")[3];
+const wdth = pathname.split("?")[1];
+const hight = pathname.split("?")[2];
+let maze=new Array(wdth*hight);
 
 function setup(){
-createCanvas(windowWidth,windowHeight);
-  for(let i=0;i<width;i++){
-    for(int j=0;j<15;j++){
-      maze[i][j] = maz.substring(i+(j*15),(i+(j*15)+1))
-    }
+createCanvas(10*wdth,10*hight);
+  for(let i=0;i<wdth*hight;i++){
+maze[i] = maz.substring(i,i+1);
   }
   console.log(maze);
 }
 
 function draw(){
+background(51);
+  showMaze(maze);
+}
 
+
+function showMaze(m) {
+  for(int i=0;i<wdth;i++){
+   for(int j=0;j<hight;j++){
+    switch(m[i*j+wdth]==0){
+      case(1):
+        
+        break;
+        case(2):
+        
+        break;
+        case(3):
+        
+        break;
+        case(4):
+        
+        break;
+        case(5):
+        
+        break;
+        case(6):
+        
+        break;
+        case(7):
+        
+        break;
+        case(8):
+        
+        break;
+        case(9):
+        
+        break;
+        case(a):
+        
+        break;
+        case(b):
+        
+        break;
+   }
+  }
 }
