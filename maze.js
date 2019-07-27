32ffaf1a247ec96dbe4fc8bba2efaebf73ab5b3e
Metype <39036543+metype@users.noreply.github.com>
2019-07-26 21:03:23 -0500

    Update maze.js

--- maze.js
+++ maze.js
@@ -5,7 +5,7 @@ const hight = pathname.split("?")[2];
 let maze=new Array(wdth*hight);
 
 function setup(){
-createCanvas(10*wdth,10*hight);
+createCanvas(40*wdth,40*hight);
   for(let i=0;i<wdth*hight;i++){
 maze[i] = maz.substring(i,i+1);
   }
@@ -23,34 +23,42 @@ function showMaze(m) {
    for(let j=0;j<hight;j++){
     switch(m[i*j+wdth]==0){
       case(1):
-        
+        line(i+20,J,i+20,j+20);
         break;
         case(2):
-        
+         line(i,J,i+20,j);
         break;
         case(3):
-        
+         line(i,J,i,j+20);
         break;
         case(4):
-        
+         line(i,J+20,i+20,j+20);
         break;
         case(5):
-        
+         line(i+20,J,i+20,j+20);
+         line(i,J+20,i+20,j+20);
         break;
         case(6):
-        
+         line(i+20,J,i+20,j+20);
+         line(i,J,i+20,j+20);
         break;
         case(7):
-        
+         line(i+20,J,i+20,j+20);
+         line(i,J+20,i+20,j+20);
         break;
         case(8):
-        
+        line(i+20,J,i+20,j+20);
+         line(i,J,i+20,j+20);
         break;
         case(9):
-        
+        line(i+20,J,i+20,j+20);
+         line(i,J,i+20,j+20);
+        line(i,J+20,i+20,j+20);
         break;
         case('a'):
-        
+        line(i,J,i+20,j+20);
+         line(i,J,i+20,j+20);
+        line(i,J+20,i+20,j+20);
         break;
         case('b'):
         
